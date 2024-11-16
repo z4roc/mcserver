d9ea48f650fd0838a6118f3dc0d72b2e348fcc2d
LetsDrinkSomeTea <github@faigle.dev>
2024-11-16 12:54:41 +0100

    Added VanillaCard for configuratable deployment of Vanilla instance.
    Modified Popup at deployment to show Port

--- app/servers/page.tsx
+++ app/servers/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 
-import { Modcard, FeaturedCard } from "@/components/Modcard";
-import AppNavBar from "@/components/Navigation";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
-import { getFeaturedModpacks, getGames, getMods } from "@/lib/curseforge_api";
+import { Modcard } from "@/components/Modcard";
+import { getFeaturedModpacks, getMods } from "@/lib/curseforge_api";
 import type { Featured, Modpack } from "@/types/modpack";
 import { useEffect, useState } from "react";
-import Image from "next/image";
-import { deployTestModpack } from "@/lib/deploy_modpack";
 import { Input } from "@/components/ui/input";
+import {VanillaCard} from "@/components/VanillaCard";
 
 export default function Modpacks() {
   const [modpacks, setModpacks] = useState<Modpack[] | null>(null);
@@ -55,32 +46,3 @@ export default function Modpacks() {
   );
 }
 
-function VanillaCard() {
-  return (
-    <Card>
-      <CardHeader>
-        <div className="flex items-center gap-4">
-          <Image
-            src={"/minecraft-icon-512.png"}
-            height={64}
-            width={64}
-            alt="mcicon"
-            className="rounded-md object-contain h-[64px] w-[64px]"
-          />
-          <h1 className="text-2xl font-bold">{"Vanilla"}</h1>
-          <Button
-            variant="default"
-            className="ml-auto"
-            onClick={() => deployTestModpack({ slug: "vanilla" } as Modpack)}
-          >
-            Deploy
-          </Button>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <p>{"The default experience of Minecraft"}</p>
-      </CardContent>
-      <CardFooter></CardFooter>
-    </Card>
-  );
-}
